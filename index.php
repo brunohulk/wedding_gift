fb6d48bf3e9797a4692c5aa3ba5f30371e85c80a
Bruno Borges <bruno@iborges.com>
2016-12-06 21:17:13 -0200

    Melhorias na versão mobile

--- index.php
+++ index.php
@@ -11,12 +11,14 @@ require_once "rate.php";
 </head>
 <body>
     <div class="row">
-        <div class="col-sm-4 col-md-4 col-lg-4 col-md-offset-4">
-            <h1>
-                <i class="fa fa-plane" aria-hidden="true"></i><i class="fa fa-gift" aria-hidden="true"></i>
-                Bruno & Andréia
-                <i class="fa fa-plane" aria-hidden="true"></i><i class="fa fa-gift" aria-hidden="true"></i>
-            </h1>
+        <div class="col-sm-4 col-md-4 col-lg-4 col-md-offset-4 col-sm-offset-4">
+            <div class="title">
+                <h1>
+                    <i class="fa fa-plane" aria-hidden="true"></i><i class="fa fa-gift" aria-hidden="true"></i>
+                    Bruno & Andréia
+                    <i class="fa fa-plane" aria-hidden="true"></i><i class="fa fa-gift" aria-hidden="true"></i>
+                </h1>
+            </div>
             <table class="table table-striped table-bordered">
                 <tr>
                     <th>
@@ -59,10 +61,15 @@ require_once "rate.php";
                     <label for="exampleInputName2">Euros</label>
                     <input type="text" readonly class="form-control" id="euroResult" placeholder="00.00">
                 </div>
-                <button type="button" id="btnCalc" class="btn btn-default">Calcular</button>
+                <button type="button" id="btnCalc" class="btn btn-default">Calcular</button><br>
+                <div class="pull-right well well-sm">
+                    1 euro = R$<?php echo number_format($euroRate, 2);?>*
+                </div>
             </form>
         </div>
+
     </div>
+
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://use.fontawesome.com/2c5e9f06d0.js"></script>
